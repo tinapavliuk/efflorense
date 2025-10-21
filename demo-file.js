b3dbb1d8ea3ea2a9878c193c053d0aca7ba1047d
efflorense <efflorense@icloud.com>
2025-10-21 19:59:16 +0300

    .

--- demo-file.js
+++ demo-file.js
@@ -1,8 +1,10 @@
 // Демонстраційний файл з поганим форматуванням
-const   user={
-name:"John",age:30,city:"Kyiv"
+const user = {
+  name: 'John',
+  age: 30,
+  city: 'Kyiv',
 };
-function   greet(  name  ){
-return   "Hello, " + name + "!";
+function greet(name) {
+  return 'Hello, ' + name + '!';
 }
 console.log(greet(user.name));
